d8af7d3e8ffa356ef630fe90db4fa0f1bd21e60e
s32974 <s32974@pjwstk.edu.pl>
2025-04-15 19:53:07 +0200

    zadanie3

---
+++ src/Student.java
@@ -0,0 +1,29 @@
+public class Student {
+
+    private String fname;
+    private String lname;
+    private int indexNumber;
+    private String email;
+    private String address;
+    public double[] grades = new double[20];
+
+    private StudentGroup studentGroup;
+
+    public void averageCalculate() {
+
+        this.grades = new double[]{3, 5, 4.5};
+
+        double sum = 0.0;
+        for (int i = 0; i < grades.length; i++) {
+            sum += grades[i];
+        }
+        if (sum == 0.0) {
+            throw new IllegalArgumentException();
+        }
+        double average = Math.ceil(sum / grades.length);
+
+        System.out.println(average);
+
+
+    }
+}
